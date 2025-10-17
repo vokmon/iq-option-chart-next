b6fa0aad39a159ac3d5e9e7d2f5b901058a2692f
Arnon Ruangthanawes <arnon.ruangthanawes@screencloud.io>
2025-10-17 19:32:32 +0700

    Remove unused hooks

--- src/components/initializeation/LoadDailyBalanceComponent.tsx
+++ src/components/initializeation/LoadDailyBalanceComponent.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { useDailyBalanceSnapshot } from "@/hooks/useDailyBalanceSnapshot";
-import { useAmountHistoryStore } from "@/stores/assets/amountHistoryStore";
 
 export default function LoadDailyBalanceComponent() {
-  useAmountHistoryStore();
   useDailyBalanceSnapshot();
   return null;
 }
